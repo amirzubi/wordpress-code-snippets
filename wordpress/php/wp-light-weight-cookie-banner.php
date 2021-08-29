fa25ca5070d63b22627f912c11fec2b640e8bfba
Amir <pascalazubi@gmail.com>
2021-08-29 09:01:09 +0200

    Added function "Light Weight Cookie Banner"

---
+++ wordpress/php/wp-light-weight-cookie-banner.php
@@ -0,0 +1,7 @@
+<?php
+add_action( 'wp_footer', function () { ?>
+<p id="cookie-notice">Our website uses cookies. If you continue to visit our website, you agree to this. You can find more information about the cookies we use in our <a href="/data-policy/"> privacy policy </a>.<br><button onclick="acceptCookie();">Alright!</button></p>
+<style>#cookie-notice{z-index: 1000 !important;color:#000000;background:#ffffff;padding:30px;position:fixed;bottom:10px;left:10px;width:100%;max-width:400px;box-shadow:0 10px 30px rgba(0,0,0,.25);border-radius:12px;margin:0px;visibility:hidden;z-index:1000000000!important;box-sizing:border-box;font-size:15px;}#cookie-notice button{line-height:15px!important;color:#ffffff;font-size:15px!important;padding:15px 24px;font-weight:500;background:#3db76f;border:0;margin-top:10px;width:100%;cursor:pointer;}@media only screen and (max-width:600px){#cookie-notice{max-width:100%;bottom:0;left:0;border-radius:0}}</style>
+<script>function acceptCookie(){document.cookie="cookieaccepted=1; expires=Thu, 18 Dec 2030 12:00:00 UTC; path=/",document.getElementById("cookie-notice").style.visibility="hidden"}document.cookie.indexOf("cookieaccepted")<0&&(document.getElementById("cookie-notice").style.visibility="visible");</script>
+<?php } );
+?>
